b22ca89ffc0f033304cdfa70f1df7575483ce785
GSoster <guilhermesosters@gmail.com>
2017-03-15 18:25:32 -0300

    added camera script so now the camera follows the player and added walls to the player area

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;//the difference betwen the transform position of the mainCamera and the transform position of the player
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	/**
+     * before each frame the camera position will be ajusted to the player position + offset.
+     **/
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
