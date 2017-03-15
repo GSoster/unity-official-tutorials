30b1bdb0189f38a33a0661b274e58477454ff9e0
Guilherme Soster <guilhermesosters@gmail.com>
2017-03-15 20:15:57 -0300

    imported in the new unity version (5.5.2f1), created collectable Objects from prefab, added material to them

---
+++ Roll a Ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+        //make the cube spin
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
