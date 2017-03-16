8a47aa9207c073f79398641041abb2de8eb6f688
Guilherme Soster <guilhermesosters@gmail.com>
2017-03-15 22:21:54 -0300

    Roll a ball: Added UI text to count user points and display WIN message

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI; //namespace for UI components
 public class PlayerController : MonoBehaviour
 {
+    public const int MAX_COLLECTABLE_OBJECTS = 12;
 
     public float speed;
+    public Text countText;//ui element
+    public Text winText;//ui element
 
     private Rigidbody rb;
+    private int count;//points that the player has picked up
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -28,7 +35,18 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
             //Destroy(other.gameObject);//we don't want to destroy it, just inativate
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= MAX_COLLECTABLE_OBJECTS)
+        {
+            winText.text = "You Win!";
         }
     }
 
