ff0d2d8a1d083484c2e7932743a43f50c9b6c1f8
GSoster <guilhermesosters@gmail.com>
2017-05-28 15:47:48 -0300

    Space Shooter: added gameObject for shots/bolts and made them become prefabs

---
+++ Space Shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		if (speed == 0 || speed == null) {
+			speed = 20;
+		}
+
+		// The z Axis is also known as Transform Forward
+		rb.velocity = transform.forward * speed;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
