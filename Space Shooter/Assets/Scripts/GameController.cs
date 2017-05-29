58ad1309776de0c35e871d4e84397d2090189c80
GSoster <guilhermesosters@gmail.com>
2017-05-29 16:39:04 -0300

    Space Shooter: added a GameController object/script that instantiates new Hazards (Asteroids)

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	//this script is responsible for spawning the hazards
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+
+
+	//it is called in the first frame when this object is instantiated
+	void Start(){
+		SpawnWaves ();
+	}
+
+	void SpawnWaves()
+	{
+		Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x),spawnValues.y,spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity;//Quaternion.identity means no rotation
+		Instantiate (hazard, spawnPosition, spawnRotation);
+	}
+
+}
