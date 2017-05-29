e81cddeb5f8c117ec191bd3b54ac1919163e5d83
GSoster <guilhermesosters@gmail.com>
2017-05-29 15:39:36 -0300

    Space Shooter: added Asteroid with two new scripts: one responsible for the random rotation and other for destroying the asteroid on contact. also fixed the boundary

---
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		//Debug.Log (other.name);//debugging
+		if(other.tag == "Boundary"){//we will not destroy if the contact is with Boundary
+			return;
+		}
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
---
+++ Space Shooter/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble;//tremer,girar
+	private Rigidbody rb;
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		//Random.insideUnitSphere gives a random vector3
+		rb.angularVelocity  = Random.insideUnitSphere * tumble;//angularVelocity is how fast a gameobject is rotating
+
+	}
+}
