ee2322d2c63cfce29ae3cb4f76f867d2555a2fc5
GSoster <guilhermesosters@gmail.com>
2017-05-29 23:17:35 -0300

    Space Shooter: added Hazard waves (Asteroids) to the game with interval among them. Also fixed Explosions, now they will be destroyed after some time

---
+++ Space Shooter/Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour {
+
+	public float lifeTime;//used to determine how long he object will wait to be destroyed
+
+	void Start(){
+		//the second parameter of Destroy is a time to execute the action of destruction.
+		Destroy (gameObject, lifeTime);
+	}
+}
--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -7,18 +7,30 @@ public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public Vector3 spawnValues;
-
+	public float hazardCount;
+	public float spawnWait;//wait time between one spawn hazard and another
+	public float startWait;//wait time when the game first start so the player can get ready;
+	public float waveWait;//how much time we await between waves
 
 	//it is called in the first frame when this object is instantiated
 	void Start(){
-		SpawnWaves ();
+		StartCoroutine (SpawnWaves ());
 	}
 
-	void SpawnWaves()
+	//coroutines need to return an IEnumerator
+	IEnumerator SpawnWaves()
 	{
-		Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x),spawnValues.y,spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;//Quaternion.identity means no rotation
-		Instantiate (hazard, spawnPosition, spawnRotation);
+		yield return new WaitForSeconds (startWait);//wait time in the start of the game, to the player get ready
+		while(true){
+			for(int i = 0; i < hazardCount; i++){
+				Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x),spawnValues.y,spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;//Quaternion.identity means no rotation
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);//coroutine, wait time between new Asteroids
+			}	
+			yield return new WaitForSeconds (waveWait);//wait time between waves
+		}
+
 	}
 
 }
