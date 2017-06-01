5bc56a976c3dc90956f32d48d9de4a564de8cf12
GSoster <guilhermesosters@gmail.com>
2017-05-31 23:38:29 -0300

    Space Shooter: added enemy ship to game, it still necessary to make the enemy shoot

---
+++ Space Shooter/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	//this script allows the enemy to shoot
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+	public float waitTime;//a time to the player gets ready
+	public bool shouldShoot;
+
+	private AudioSource audioSource;//sound from the shot/bolt
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource >();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//while (shouldShoot) {
+			if (Time.time > nextFire)
+			{
+				nextFire = Time.time + fireRate;
+				//Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+				//audioSource.Play ();
+			}
+		//}
+	}
+}
