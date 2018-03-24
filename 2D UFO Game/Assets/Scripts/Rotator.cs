2c995a121786b5ae198081f20ee1ef4f9e8e0c72
GSoster <guilhermesosters@gmail.com>
2018-03-24 15:38:00 -0300

    Created pickups, created script to rotate them and made the pickups into prefabs

---
+++ 2D UFO Game/Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		// rotates around the Z axys
+		// multiplies by Time.deltaTime to be a smooth rotation
+		transform.Rotate (new Vector3 (0, 0, 45) * Time.deltaTime);	
+	}
+}
