8d181e3ca66588ff4f7970a9ed0bf4571b4997e5
GSoster <guilhermesosters@gmail.com>
2018-03-24 15:06:03 -0300

    added physics 2d and movement to the player

---
+++ 2D UFO Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rb2d;
+
+
+
+	void Start ()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+
+	//Physics code
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+	}
+}
